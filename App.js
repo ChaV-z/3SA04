9745bfc879b1b60f43b979c3712659019a02f20a
ChaV-z <yong.christmas@gmail.com>
2021-07-15 09:52:11 +0700

    15 min

--- App.js
+++ App.js
@@ -1,11 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+import Weather from './components/Weather';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Weather zipCode="95110"/>
       <StatusBar style="auto" />
     </View>
   );
